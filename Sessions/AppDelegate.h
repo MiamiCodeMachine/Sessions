20b97844f42faf41f175d32c1acd462f7a3c6426
Carlos Duran <afg504@gmail.com>
2015-04-13 10:21:39 -0400

    Initial Commit

---
+++ Sessions/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Sessions
+//
+//  Created by Carlos Duran on 4/13/15.
+//  Copyright (c) 2015 Mobiliture. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Sessions/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Sessions
+//
+//  Created by Carlos Duran on 4/13/15.
+//  Copyright (c) 2015 Mobiliture. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
