34b7da20523c85a323b59832c6916df1f9990e18
Carlos Duran <afg504@gmail.com>
2015-04-15 07:32:28 -0400

    Initial commit

--- Sessions/ViewController.m
+++ Sessions/ViewController.m
@@ -8,16 +8,58 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <NSURLSessionDelegate,NSURLSessionDownloadDelegate>
+
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (weak, nonatomic) IBOutlet UIProgressView *progressView;
 
 @end
 
 @implementation ViewController
 
+- (void)URLSession:(NSURLSession *)session downloadTask:(NSURLSessionDownloadTask *)downloadTask didFinishDownloadingToURL:(NSURL *)location {
+    NSData *data = [NSData dataWithContentsOfURL:location];
+    
+    dispatch_async(dispatch_get_main_queue(), ^{
+//        [self.progressView setHidden:YES];
+        [self.imageView setImage:[UIImage imageWithData:data]];
+    });
+}
+
+- (void)URLSession:(NSURLSession *)session downloadTask:(NSURLSessionDownloadTask *)downloadTask didResumeAtOffset:(int64_t)fileOffset expectedTotalBytes:(int64_t)expectedTotalBytes {
+    
+}
+
+- (void)URLSession:(NSURLSession *)session downloadTask:(NSURLSessionDownloadTask *)downloadTask didWriteData:(int64_t)bytesWritten totalBytesWritten:(int64_t)totalBytesWritten totalBytesExpectedToWrite:(int64_t)totalBytesExpectedToWrite {
+    float progress = (double)totalBytesWritten / (double)totalBytesExpectedToWrite;
+    
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [self.progressView setProgress:progress];
+    });
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
+
+    NSURLSessionConfiguration *sessionConfiguration = [NSURLSessionConfiguration defaultSessionConfiguration];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:sessionConfiguration delegate:self delegateQueue:nil];
+    NSURLSessionDownloadTask *downloadTask = [session downloadTaskWithURL:[NSURL URLWithString:@"http://cdn.tutsplus.com/mobile/uploads/2013/12/sample.jpg"]];
+    [downloadTask resume];
+    
+    
+//  Example 1
+//    NSString *urlAString = @"https://itunes.apple.com/search?term=apple&media=software";
+//    NSURLSession *session = [NSURLSession sharedSession];
+//    
+//    NSURLSessionDataTask *dataTask = [session dataTaskWithURL:[NSURL URLWithString:urlAString]
+//                                            completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+//
+//                                                NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+//                                                NSLog(@"json : %@",json);
+//
+//    }];
+//    [dataTask resume];
+    
+};
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
